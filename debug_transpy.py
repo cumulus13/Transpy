80554b4bd45b95be2057088cf64c6773b4aeae82
cumulus13 <cumulus13@gmail.com>
2025-10-27 13:28:44 +0700

    version: 1.0.4 ~ 2025-10-27 13:28:38:581365

--- debug_transpy.py
+++ debug_transpy.py
@@ -3,18 +3,18 @@ import sublime_plugin
 
 class TranspyDebugCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        print("🔧 Transpy: Debug command executed!")
+        print("ðŸ”§ Transpy: Debug command executed!")
         sublime.status_message("Transpy: Debug command working!")
         
         # Test selection
         selections = self.view.sel()
-        print(f"Selections: {len(selections)}")
+        print("Selections:", len(selections))
         
         for i, region in enumerate(selections):
             if region.empty():
                 line = self.view.line(region)
                 text = self.view.substr(line)
-                print(f"Selection {i}: LINE - '{text}'")
+                print(f"Selection", i, ": LINE - '" + text + "'")
             else:
                 text = self.view.substr(region)
-                print(f"Selection {i}: TEXT - '{text}'")
+                print("Selection", i, ": TEXT - '%s'" % text)
